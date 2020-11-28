f7bbb0c4f69b806467ac30d2b070bbb7c162f742
adriangohjw <adriangohjw@gmail.com>
2020-11-28 16:38:28 +0800

    Update SQLParser

--- sql_parser.py
+++ sql_parser.py
@@ -1,12 +1,79 @@
+import itertools
 import sqlparse
 
-def parseSplitSQL(raw_sql_query):
+from sqlparse.sql import IdentifierList, Identifier
+from sqlparse.tokens import Keyword, DML
 
-    statements = sqlparse.split(raw_sql_query)
-    statement = statements[0]
+from column_selector import ColumnSelector
 
-    sql_parsed = sqlparse.format(statement, reindent=True, keyword_case='upper')
-    sql_parsed_split = sql_parsed.splitlines()
 
-    return sql_parsed_split
+class SQLParser:
+    def __init__(self, query):
+        self.query = query
+        self.tables = self.__extract_tables()
+        self.selectable_columns = self.__get_selectable_columns()
+        self.splitted_query = self.__splitted()
+
+
+    def __splitted(self):
+        statements = sqlparse.split(self.query)
+        statement = statements[0]
+        sql_parsed = sqlparse.format(statement, reindent=True, keyword_case='upper')
+        sql_parsed_split = sql_parsed.splitlines()
+        return sql_parsed_split
+
     
+    def __get_selectable_columns(self):
+        selectable_columns = {}
+        for table in self.tables:
+            selectable_columns[table] = ColumnSelector(table).get_attributes()
+        return selectable_columns
+
+
+    def __extract_tables(self):
+        extracted_tables = []
+        statements = list(sqlparse.parse(self.query))
+        for statement in statements:
+            if statement.get_type() != 'UNKNOWN':
+                stream = self.__extract_from_part(statement)
+                extracted_tables.append(set(list(self.__extract_table_identifiers(stream))))
+        return list(itertools.chain(*extracted_tables))
+
+
+    def __is_subselect(self, parsed):
+        if not parsed.is_group:
+            return False
+        for item in parsed.tokens:
+            if item.ttype is DML and item.value.upper() == 'SELECT':
+                return True
+        return False
+
+
+    def __extract_from_part(self, parsed):
+        from_seen = False
+        for item in parsed.tokens:
+            if item.is_group:
+                for x in self.__extract_from_part(item):
+                    yield x
+            if from_seen:
+                if self.__is_subselect(item):
+                    for x in self.__extract_from_part(item):
+                        yield x
+                elif item.ttype is Keyword and item.value.upper() in ['ORDER', 'GROUP', 'BY', 'HAVING', 'GROUP BY']:
+                    from_seen = False
+                    StopIteration
+                else:
+                    yield item
+            if item.ttype is Keyword and item.value.upper() == 'FROM':
+                from_seen = True
+
+
+    def __extract_table_identifiers(self, token_stream):
+        for item in token_stream:
+            if isinstance(item, IdentifierList):
+                for identifier in item.get_identifiers():
+                    value = identifier.value.replace('"', '').lower()                
+                    yield value
+            elif isinstance(item, Identifier):
+                value = item.value.replace('"', '').lower()
+                yield value
